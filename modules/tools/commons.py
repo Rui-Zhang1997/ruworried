7ace78c0fc0c0c09674b803431be8eea41e926b9
Rui-Zhang1997 <r.zhang194@gmail.com>
2017-10-06 12:00:49 -0400

    Parser is finished (?) Needs more testing. But is almost there

--- modules/tools/commons.py
+++ modules/tools/commons.py
@@ -15,3 +15,18 @@ def reduce(lmda, lst, acc=None):
     for i in range(s, len(lst)):
         acc = lmda(acc, lst[i])
     return acc
+
+def categorize(lst, *args):
+    a = [[] for i in range(len(args))]
+    rem = []
+    for l in lst:
+        no_match = True
+        for i in range(len(args)):
+            fn = args[i]
+            if fn(l):
+                a[i].append(l)
+                no_match = False
+                continue
+        if no_match:
+            rem.append(l)
+    return (a, rem)
--- modules/tools/parser.py
+++ modules/tools/parser.py
@@ -32,8 +32,9 @@ class Crime:
 
 C_INCIDENT_PATTERN = '[0-9]{2}-[0-9]{6}'
 def extrapolate_row_data(row_data):
+    print(row_data)
     datePattern = re.compile('^[0-9]{2}/[0-9]{2}/[0-9]{2}.*')
-    timePattern = re.compile('.*[0-9]{4}hrs.*')
+    timePattern = re.compile('.*[0-9]{4}(hrs|Hrs).*')
     idPattern   = re.compile(C_INCIDENT_PATTERN)
     id_ = []
     for d in row_data:
@@ -41,30 +42,31 @@ def extrapolate_row_data(row_data):
             id_ += d.split('\n')
     del row_data[row_data.index('\n'.join(id_))]
     incident_count = len(id_)
-    dates = []
-    texts = []
     crime = Crime()
     crime.crime_id = id_[0]
     if incident_count > 1: # processes 1-to-many
+        texts = []
+        dates = []
         for d in row_data:
             if datePattern.search(d):
                 dates.append(d)
             else:
-                text.append(d)
-        dates = flatten([date.split('\n') for date in dates])
-        texts = flatten([text.split('\n') for text in texts])
+                texts.append(d)
+        dates = cms.flatten([date.split('\n') for date in dates])
+        texts = cms.flatten([text.split('\n') for text in texts])
         dt_tuples = [(d,t) for d,t in zip(dates,texts)]
         for dt_tuple in dt_tuples:
             date = dt_tuple[0].split(' ')
             times_list = date[:4]
             # sorting datetimes to determine report status
             # joins both strings together and removes all nonnumeric characters
+            print("TL", times_list)
             dt1 = re.sub('[^0-9]', '', ''.join(times_list[:2]).lower())
-            dt2 = re.sub('[^0-9]', '', ''.join(times_list[2:4]).lower())
+            dt2 = re.sub('[^0-9]', '', ''.join(times_list[2:4]).lower()) if len(times_list) > 2 else None
             # converts them to DateTime object
-            dts = [lambda dt: DateTime(dt[:2], dt[2:4], dt[4:6], dt[6:8], dt[8:]) if dt else None for dt in [dt1, dt2]]
+            dts = [DateTime(dt[:2], dt[2:4], dt[4:6], dt[6:8], dt[8:]) if dt else None for dt in [dt1, dt2]]
             # sorts them based on latest to earliest
-            dts = dts if dts[0] and dts[1] and dts[0].compare(dts[1]) == 1 else [dts[1], dts[0]]
+            dts = dts if not dts[0] or not dts[1] and dts[0].compare(dts[1]) == 1 else [dts[1], dts[0]]
             crime.report_time = dts[0]
             crime.occur_time = dts[1]
             crime.crime_type = dt_tuple[1]
@@ -78,12 +80,25 @@ def extrapolate_row_data(row_data):
                     crime.location = text[:def_range.start()]
                     break # if one is found, then break for everything else
     elif incident_count == 1: # processes 1-to-1
-        for d in row_date:
-            if datePattern.search(d):
-                dates.append(d)
-            else:
-                text.append(d)
-    return crime
+        data = cms.flatten([rd.split(' ') for rd in row_data])
+        dtdata, texts = cms.categorize(data, lambda n: datePattern.search(n) or timePattern.search(n))
+        dtdata = cms.flatten(dtdata)
+        dt_tuples = [(dtdata[i], dtdata[i+1]) for i in range(0, len(dtdata), 2)]
+        dt1 = re.sub('[^0-9]', '', ''.join(dt_tuples[0]))
+        dt2 = re.sub('[^0-9]', '', ''.join(dt_tuples[1])) if len(dt_tuples) > 1 else None
+        dt_sorted = [DateTime(dt[:2], dt[2:4], dt[4:6], dt[6:8], dt[8:]) if dt else None for dt in [dt1, dt2]]
+        dt_sorted = dt_sorted if (not dt_sorted[0]) or (not dt_sorted[1]) or dt_sorted[0].compare(dt_sorted[1]) == 1 else [dt_sorted[1], dt_sorted[0]]
+        crime.report_time = dt_sorted[0]
+        crime.occur_time = dt_sorted[1]
+        for text in texts:
+            for status in dfs.STATUSES:
+                if status in text:
+                    crime.status = status
+            if not crime.status or crime.status.strip() == '':
+                crime.crime_type = text
+    print(crime.__dict__)
+    print()
+    return None
 
 def parse_html(html_dir, html_file):
     html_path = path.join(html_dir, html_file)
